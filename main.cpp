33c1e648555eb96046b59ae9c6724082219712db
Juhwan Jung <remagman@gmail.com>
2023-11-28 00:31:55 -0500

    Commiting all files

---
+++ main.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+using namespace std;
+int player = 1;
+const int rows = 6;
+const int cols = 7;
+bool done = true;
+int board[6][7];
+int visited[6][7];
+int input;
+
+int init(){
+    for (int r = 0; r < rows; r++){
+        for (int c = 0; c < cols; c++){
+            board[r][c] = 0;
+            visited[r][c] = false;
+        }
+    }
+    return -1;
+}
+
+int ldfs(int r, int c, int p){
+    if (board[r][c-1] == p && r < rows && c < cols && !visited[r][c]){
+        visited[r][c] = true;
+        return 1 + ldfs(r,c-1,p);
+}
+    return 0;
+}
+int rdfs(int r, int c, int p){
+    if (board[r][c+1] == p && r < rows && c < cols && !visited[r][c]){
+        visited[r][c] = true;
+        return 1 + rdfs(r,c+1,p);
+    }
+    return 0;
+}
+int udfs(int r, int c, int p){
+    if (board[r+1][c] == p && r < rows && c < cols && !visited[r][c]){
+        visited[r][c] = true;
+        return 1 + udfs(r+1,c,p);
+    }
+    return 0;
+}
+int ddfs(int r, int c, int p){
+    if (board[r-1][c] == p && r < rows && c < cols && !visited[r][c]){
+        visited[r][c] = true;
+        return 1 + ddfs(r-1,c,p);
+    }
+    return 0;
+}
+int dudfs(int r, int c, int p){
+    if (board[r+1][c+1] == p && r < rows && c < cols && !visited[r][c]){
+        visited[r][c] = true;
+        return 1 + dudfs(r+1,c+1,p);
+    }
+    return 0;
+}
+int dddfs(int r, int c, int p){
+    if (board[r-1][c-1] == p && r < rows && c < cols && !visited[r][c]){
+        visited[r][c] = true;
+        return 1 + dddfs(r-1,c-1,p);
+    }
+    return 0;
+}
+int drdfs(int r, int c, int p){
+    if (board[r-1][c+1] == p && r < rows && c < cols && !visited[r][c]){
+        visited[r][c] = true;
+        return 1 + drdfs(r-1,c+1,p);
+    }
+    return 0;
+}
+int dldfs(int r, int c, int p){
+    if (board[r+1][c-1] == p && r < rows && c < cols && !visited[r][c]){
+        visited[r][c] = true;
+        return 1 + dldfs(r+1,c-1,p);
+    }
+    return 0;
+}
+
+
+//bool check(int r, int c, int p) {
+//    if (board[r+1][c] == p)
+//}
+
+void draw(){
+    for (int r = 0; r < rows; r++){
+        for (int c = 0; c < cols; c++){
+            cout<<board[r][c]<<" ";
+            visited[r][c] = false;
+        }
+        cout<<'\n';
+    }
+
+}
+
+int choose(){
+    while (done) {
+        cout << "Player: " << player << " Enter from 1 - 7:\n";
+        cin >> input;
+
+        input--;
+        cout << "\nThis is the input: " << input <<"\n";
+        if (board[0][input] != 0){
+            cout << "Full Column please select another number\n";
+            draw();
+            continue;
+        }
+
+        int count = 0;
+
+        while (count < 6) {
+            if (board[count][input] != 0){
+                break;
+            }
+            count++;
+        }
+
+        board[count-1][input] = player;
+
+        if (ldfs(count-1,input,player) > 2 ||
+            rdfs(count-1,input,player) > 2 ||
+            udfs(count-1,input,player) > 2 ||
+            ddfs(count-1,input,player) > 2 ||
+            dudfs(count-1,input,player) > 2 ||
+            dddfs(count-1,input,player) > 2 ||
+            drdfs(count-1,input,player) > 2 ||
+            dldfs(count-1,input,player) > 2 ){
+            draw();
+            cout << "Game Over! Player: " << player << " wins";
+            return -1;
+        }
+        draw();
+        if (player == 1){
+            player = 2;
+        }
+        else{
+            player = 1;
+        }
+    }
+
+}
+
+
+int main() {
+    init();
+    draw();
+    choose();
+    return 0;
+}
