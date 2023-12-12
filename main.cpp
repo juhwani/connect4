02dc17cddd391493740cfa36ea970f142366086a
Juhwan Jung <remagman@gmail.com>
2023-12-12 00:49:03 -0500

    Created new Dummy bot feature where AI function randomly decides between 1 - 7 to place the piece. See if you are good enough to beat this dummy bot

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <random>
+
 using namespace std;
 int player = 1;
 const int rows = 6;
@@ -75,10 +77,13 @@ int dldfs(int r, int c, int p){
     return 0;
 }
 
+int AI(){
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<double> dist(1.0, 7.0);
 
-//bool check(int r, int c, int p) {
-//    if (board[r+1][c] == p)
-//}
+    return dist(mt);
+}
 
 void draw(){
     for (int r = 0; r < rows; r++){
@@ -92,49 +97,113 @@ void draw(){
 }
 
 int choose(){
-    while (done) {
-        cout << "Player: " << player << " Enter from 1 - 7:\n";
-        cin >> input;
+    int pmode = 0;
+    cout << "Please Enter Your Choice:\n"<<"1. PvP\n2. Dummy Bot\n";
+    cin >> pmode;
+    switch(pmode){
+        case 1:
+            while (done) {
+                cout << "Player: " << player << " Enter from 1 - 7:\n";
+                cin >> input;
 
-        input--;
-        cout << "\nThis is the input: " << input <<"\n";
-        if (board[0][input] != 0){
-            cout << "Full Column please select another number\n";
-            draw();
-            continue;
-        }
+                input--;
+                cout << "\nThis is the input: " << input <<"\n";
+                if (board[0][input] != 0){
+                    cout << "Full Column please select another number\n";
+                    draw();
+                    continue;
+                }
 
-        int count = 0;
+                int count = 0;
 
-        while (count < 6) {
-            if (board[count][input] != 0){
-                break;
+                while (count < 6) {
+                    if (board[count][input] != 0){
+                        break;
+                    }
+                    count++;
+                }
+
+                board[count-1][input] = player;
+
+                if (ldfs(count-1,input,player) > 2 ||
+                    rdfs(count-1,input,player) > 2 ||
+                    udfs(count-1,input,player) > 2 ||
+                    ddfs(count-1,input,player) > 2 ||
+                    dudfs(count-1,input,player) > 2 ||
+                    dddfs(count-1,input,player) > 2 ||
+                    drdfs(count-1,input,player) > 2 ||
+                    dldfs(count-1,input,player) > 2 ){
+                    draw();
+                    cout << "Game Over! Player: " << player << " wins";
+                    return -1;
+                }
+                draw();
+                if (player == 1){
+                    player = 2;
+                }
+                else{
+                    player = 1;
+                }
             }
-            count++;
-        }
+        case 2:
+            while (done) {
 
-        board[count-1][input] = player;
+                if (player == 1){
+                    cout << "Player: " << player << " Enter from 1 - 7:\n";
+                    cin >> input;
+                }
+                else {
+                    input = AI();
+                }
+                input--;
+                cout << "\nThis is the input: " << input <<"\n";
+                if (board[0][input] != 0){
+                    cout << "Full Column please select another number\n";
+                    draw();
+                    continue;
+                }
 
-        if (ldfs(count-1,input,player) > 2 ||
-            rdfs(count-1,input,player) > 2 ||
-            udfs(count-1,input,player) > 2 ||
-            ddfs(count-1,input,player) > 2 ||
-            dudfs(count-1,input,player) > 2 ||
-            dddfs(count-1,input,player) > 2 ||
-            drdfs(count-1,input,player) > 2 ||
-            dldfs(count-1,input,player) > 2 ){
-            draw();
-            cout << "Game Over! Player: " << player << " wins";
-            return -1;
-        }
-        draw();
-        if (player == 1){
-            player = 2;
-        }
-        else{
-            player = 1;
-        }
+                int count = 0;
+
+                while (count < 6) {
+                    if (board[count][input] != 0){
+                        break;
+                    }
+                    count++;
+                }
+
+                board[count-1][input] = player;
+
+                if (ldfs(count-1,input,player) > 2 ||
+                    rdfs(count-1,input,player) > 2 ||
+                    udfs(count-1,input,player) > 2 ||
+                    ddfs(count-1,input,player) > 2 ||
+                    dudfs(count-1,input,player) > 2 ||
+                    dddfs(count-1,input,player) > 2 ||
+                    drdfs(count-1,input,player) > 2 ||
+                    dldfs(count-1,input,player) > 2 ){
+                    draw();
+                    if (player == 0){
+                    cout << "Game Over! Player: " << player << " wins";}
+                    else {
+                        cout << "Game Over! A dummy bot just beat you.";
+                    }
+                    return -1;
+                }
+                draw();
+
+                if (player == 1){
+
+                    player = 2;
+                }
+                else{
+                    cout << "Bot just played " << input + 1<< "\n";
+                    player = 1;
+                }
+            }
+
     }
+
 
 }
 
